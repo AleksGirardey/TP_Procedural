13dedcac52a0cd0d298ac9dc172df7b67236fae0
AleksGirardey <alexis.girardey@gmail.com>
2020-05-11 11:55:59 +0200

    feat: dungeon generator

---
+++ Assets/TP_Procedural/Runtime/Scripts/DungeonGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour {
+    [Range(3, 30)]
+    public int dungeonSize;
+
+    public Node[][] _dungeonMap;
+    
+    private bool _isNewGeneration;
+
+    private void Awake() {
+        
+    }
+
+    private void Start() {
+        GenerateDungeon();
+    }
+
+    private void Update() {
+        if (_isNewGeneration)
+            DisplayDungeon();
+    }
+
+    private void DisplayDungeon() {
+
+        _isNewGeneration = false;
+    }
+
+    public void GenerateDungeon() {
+        _isNewGeneration = true;
+
+    }
+    
+}
