7c64b93d770366ebc3fecb23a3a72f0d584524c2
JeanDev <hardyjea@gmail.com>
2020-05-11 11:52:50 +0200

    Add Node and Connection classes

---
+++ Assets/TP_Procedural/Runtime/Scripts/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private enum TYPE
+    {
+        START,
+        END,
+        DOOR,
+        KEY
+    }
+    private TYPE nodeType = TYPE.START;
+
+    private int _difficulty = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
